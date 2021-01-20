c9dee9f9e7bf8a6e47801f8cc6c0cd072eafeb77
Wendy-Anna <wendyufpb@gmail.com>
2021-01-20 13:56:48 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
+import Routes from './routes';
 import HomeList from './HomeLista';
 
-function App(props) {
-
-
+function App() {
   return (
     <>
       <h1>Pokedex-Gama</h1>
-      <HomeList />
+      <Routes />
     </>
   );
 }
