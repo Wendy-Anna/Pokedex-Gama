1d75ea3df02b1df7d303070c9fc463e4c0975f71
Wendy-Anna <wendyufpb@gmail.com>
2021-01-22 15:24:24 -0300

    Linkando os botoes da home

--- src/PesquisaPokemon/index.js
+++ src/PesquisaPokemon/index.js
@@ -1,30 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import pokemon from './pokemon.jpg';
+import React from 'react';
+
 
 export default function PesquisaPokemon() {
 
-  const [searchPokemon, setSearchPokemon] = useState([]);
-
-  useEffect(() => {
-    let pokemonName = localStorage.getItem('pokemonName');
-    pokemonName = JSON.parse(pokemonName);
-    setSearchPokemon(pokemonName);
-
-  }, []);
 
   return (
     <>
       <h1>Informações do Pokemon</h1>
-      <div className="container">
-        <img src={pokemon} alt="Imagem do pokemon" />
-        <ul>
-          {searchPokemon.map(pesquisa => {
-            return (
-              <li>{pesquisa}</li>
-            )
-          })}
-        </ul>
-      </div>
     </>
   )
 }
